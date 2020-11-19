08ad666524167d54d5cc4a73a72c0593b0e5c792
3epr <60379643+3epr@users.noreply.github.com>
2020-11-19 09:41:23 +0300

    complete 2

--- 02_log_parser.py
+++ 02_log_parser.py
@@ -18,8 +18,43 @@
 #
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+from pprint import pprint
 
-# TODO здесь ваш код
+
+class LogAnalizator:
+    def __init__(self, inputf, outputf=None):
+        self.name = inputf
+        self.out = outputf
+        self.st = {}
+
+    def sbor_stat(self, n=17):
+        with open(self.name, 'r', encoding='cp1251') as file:
+            for line in file:
+                if line[1:n] in self.st and line[29:-1] == 'NOK':
+                    self.st[line[1:n]] += 1
+                elif line[29:-1] == 'NOK':
+                    self.st[line[1:n]] = 1
+
+    def output(self, n=0):
+        if n == 0:
+            self.sbor_stat()
+        elif n == 1:
+            self.sbor_stat(14)
+        elif n == 2:
+            self.sbor_stat(8)
+        elif n == 3:
+            self.sbor_stat(5)
+        if self.out == None:
+            pprint(self.st)
+        else:
+            file = open(self.out, 'w', encoding='utf8')
+            for _ in self.st:
+                lin = f'[{_}] {self.st[_]}\n'
+                file.writelines(lin)
+            file.close()
+
+first = LogAnalizator('events.txt', 'out.txt')
+first.output(0)
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
